c26869f7e73c3e679e4eff87cae8b4e9c5637b18
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-28 16:59:51 +0200

    Update ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 int		ft_isdigit(int test)
 {
 	if (test <= '9' && test >= '0')
