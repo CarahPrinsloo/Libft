685bcdd369a91178b0adee4bd78153d8ec83f665
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-28 17:00:25 +0200

    Update ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 int		ft_isprint(int test)
 {
 	if (test >= 32 && test < 127)
