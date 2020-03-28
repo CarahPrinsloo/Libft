7bb6bf00c9f0a4514adf2719ede96a95fab61d72
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-28 20:45:46 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -4,8 +4,12 @@ void *ft_memcpy(void *to, const void *from, size_t numBytes)
     unsigned char *dest;
     unsigned char *word;
     
+    if (numBytes == 0)
+        return (to);
     dest = (char*) to;
     word = (char*) from;
+    if (n == 0)
+        return (dst);
     while (numBytes > 0)
     {
         *dest = *word;
