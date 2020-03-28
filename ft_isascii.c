6a48baa8c0e8daa8465583844a0a135d9be3e4ae
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-28 16:59:20 +0200

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 int		ft_isascii(int test)
 {
 	if (test >= 0 && test <= 127)
