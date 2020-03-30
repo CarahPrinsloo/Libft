99f4b32f1199580a369d62d96654b662fcd2f571
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-30 15:43:17 +0200

    Update ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -1,34 +1,17 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h>
 #include <string.h>
-
-int     str_length(char *str)
-{
-    int length;
-    int index = 0;
-    
-    length = 0;
-    while (str[index++])
-        length++;
-    return (length);
-}
 
 char *ft_strcat(char *dest, const char *src)
 {
-    int index;
-    int size;
+    int i;
+    int j;
     
-    size = str_length(dest);
-    index = 0;
-    while (src[index++])
-        dest[size++] = src[index];
+    j = strlen(dest);
+    i = 0;
+    while (src[index])
+    {
+        dest[j] = src[i];
+        i++;
+        j++;
+    }
     return (dest);
 }
-   
