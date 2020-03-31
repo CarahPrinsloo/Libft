6543e1e1200cf6f611f08abd1433c2f7e03ab071
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-03-31 15:22:58 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,7 +4,7 @@ int ft_strncmp(const char *str1, const char *str2, size_t n)
     int i;
     
     i = 0;
-    while(i < n)
+    while(((str1[i] != '\0') || (str2[i] != '\0')) && i < n)
     {
         if(str1[i] != str2[i])
             return (str1[i] - str2[i]);
